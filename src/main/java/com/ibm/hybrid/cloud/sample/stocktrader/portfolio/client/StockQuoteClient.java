960904e7b10c6be472386d6180467d5e3ea049b6
John Alcorn <jalcorn@us.ibm.com>
2018-12-05 21:06:21 -0600

    Update package

--- src/main/java/com/ibm/hybrid/cloud/sample/stocktrader/portfolio/client/StockQuoteClient.java
+++ src/main/java/com/ibm/hybrid/cloud/sample/stocktrader/portfolio/client/StockQuoteClient.java
@@ -14,7 +14,7 @@
    limitations under the License.
  */
 
-package com.ibm.hybrid.cloud.sample.portfolio;
+package com.ibm.hybrid.cloud.sample.stocktrader.portfolio.client;
 
 
 import javax.enterprise.context.Dependent;
