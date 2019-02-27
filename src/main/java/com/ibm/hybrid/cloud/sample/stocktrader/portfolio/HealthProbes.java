27cc646134067759a0749968e85e315233004abb
John Alcorn <jalcorn@us.ibm.com>
2019-02-27 14:40:31 -0500

    Roll-your-own Kubernetes readiness/liveness probes
    An alternative to the mpHealth approach, for comparison.  Pick which you want via the path you specify to the readiness and liveness probe in your deployment yaml.

---
+++ src/main/java/com/ibm/hybrid/cloud/sample/stocktrader/portfolio/HealthProbes.java
@@ -0,0 +1,68 @@
+/*
+       Copyright 2019 IBM Corp All Rights Reserved
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
+package com.ibm.hybrid.cloud.sample.stocktrader.portfolio;
+
+import com.ibm.hybrid.cloud.sample.stocktrader.portfolio.json.HealthResponse;
+
+import java.util.logging.Logger;
+
+//JAX-RS 2.0 (JSR 339)
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
+import javax.ws.rs.Path;
+import javax.ws.rs.ServiceUnavailableException; //503
+
+
+@ApplicationPath("/")
+@Path("/")
+/** Implement the Kubernetes readiness and liveness probes */
+public class HealthProbes extends Application {
+	private static Logger logger = Logger.getLogger(HealthProbes.class.getName());
+
+	@GET
+	@Path("/readiness")
+	@Produces(MediaType.APPLICATION_JSON)
+    public HealthResponse readiness() {
+        HealthResponse response = null;
+        if (PortfolioService.isReady()) {
+            logger.fine("Readiness probe succeeded");
+            response = new HealthResponse("readiness", "ready");
+        } else {
+            logger.info("Readiness probe failed");
+            throw new ServiceUnavailableException();
+        }
+        return response;
+    }
+
+	@GET
+	@Path("/liveness")
+	@Produces(MediaType.APPLICATION_JSON)
+    public HealthResponse liveness() {
+        HealthResponse response = null;
+        if (PortfolioService.isHealthy()) {
+            logger.fine("Liveness probe succeeded");
+            response = new HealthResponse("liveness", "healthy");
+        } else {
+            logger.info("Liveness probe failed");
+            throw new ServiceUnavailableException();
+        }
+        return response;
+    }
+}
