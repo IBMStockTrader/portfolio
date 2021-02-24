51a57d0174274ed337f2bf34ef709b56965a36d5
John Alcorn <jalcorn@us.ibm.com>
2021-02-23 21:19:05 -0600

    Remove fields now handled by Account

--- src/main/java/com/ibm/hybrid/cloud/sample/stocktrader/portfolio/json/Portfolio.java
+++ src/main/java/com/ibm/hybrid/cloud/sample/stocktrader/portfolio/json/Portfolio.java
@@ -1,5 +1,5 @@
 /*
-       Copyright 2017-2019 IBM Corp All Rights Reserved
+       Copyright 2017-2021 IBM Corp All Rights Reserved
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
@@ -45,14 +45,6 @@ public class Portfolio {
     @Column(nullable = false, length = 32)
     private String owner;
     private double total;
-    @Column(length = 8)
-    private String loyalty;
-    private double balance;
-    private double commissions;
-    private int free;
-    private String sentiment;
-    @Transient
-    private double nextCommission;
     @Transient
     JsonObject stocks;
 
@@ -67,16 +59,9 @@ public class Portfolio {
         setOwner(initialOwner);
     }
 
-    public Portfolio(String initialOwner, double initialTotal, String initialLoyalty, double initialBalance,
-                     double initialCommissions, int initialFree, String initialSentiment, double initialNextCommission) {
+    public Portfolio(String initialOwner, double initialTotal) {
         setOwner(initialOwner);
         setTotal(initialTotal);
-        setLoyalty(initialLoyalty);
-        setBalance(initialBalance);
-        setCommissions(initialCommissions);
-        setFree(initialFree);
-        setSentiment(initialSentiment);
-        setNextCommission(initialNextCommission);
     }
 
     public String getOwner() {
@@ -93,54 +78,6 @@ public class Portfolio {
 
     public void setTotal(double newTotal) {
         total = newTotal;
-    }
-
-    public String getLoyalty() {
-        return loyalty;
-    }
-
-    public void setLoyalty(String newLoyalty) {
-        loyalty = newLoyalty;
-    }
-
-    public double getBalance() {
-        return balance;
-    }
-
-    public void setBalance(double newBalance) {
-        balance = newBalance;
-    }
-
-    public double getCommissions() {
-        return commissions;
-    }
-
-    public void setCommissions(double newCommissions) {
-        commissions = newCommissions;
-    }
-
-    public int getFree() {
-        return free;
-    }
-
-    public void setFree(int newFree) {
-        free = newFree;
-    }
-
-    public String getSentiment() {
-        return sentiment;
-    }
-
-    public void setSentiment(String newSentiment) {
-        sentiment = newSentiment;
-    }
-
-    public double getNextCommission() {
-        return nextCommission;
-    }
-
-    public void setNextCommission(double newNextCommission) {
-        nextCommission = newNextCommission;
     }
 
     public JsonObject getStocks() {
@@ -191,8 +128,6 @@ public class Portfolio {
    }
 
     public String toString() {
-        return "{\"owner\": \""+owner+"\", \"total\": "+total+", \"loyalty\": \""+loyalty+"\", \"balance\": "+balance
-               +", \"commissions\": "+commissions+", \"free\": "+free+", \"nextCommission\": "+nextCommission
-               +", \"sentiment\": \""+sentiment+"\", \"stocks\": "+(stocks!=null?stocks.toString():"{}")+"}";
+        return "{\"owner\": \""+owner+"\", \"total\": "+total+", \"stocks\": "+(stocks!=null?stocks.toString():"{}")+"}";
     }
 }
